f313511894ddc0bac5bd7f02f9e15c503d7e1554
asnowmang <wsobs@yandex.com>
2020-10-14 23:48:52 -0500

    Optimized loop

--- Months/main.f90
+++ Months/main.f90
@@ -6,22 +6,24 @@ program months
   leap = .true.
 
   do n = 1, 12
-     if (n .eq. 2) then
-        select case(leap)
-        case(.true.)
-           month(n) = 29
-        case default
-           month(n) = 28
-        end select             
-     elseif (mod(n, 2) .eq. 0 .and. n .lt. 8) then
-        month(n) = 30
-     elseif (mod(n, 2) .ne. 0 .and. n .ge. 8) then
-        month(n) = 30
-     else
+     if (mod(n, 2) .ne. 0 .and. n .lt. 8) then     ! Pre-August, odd
         month(n) = 31
+     elseif (mod(n, 2) .eq. 0 .and. n .ge. 8) then ! Post-July, even
+        month(n) = 31
+     else
+        month(n) = 30
      endif
+  enddo
 
+  select case(leap)
+     case(.true.)
+        month(2) = 29
+     case default
+        month(2) = 28
+  end select
+
+  do n = 1, 12
      print *, month(n)
   enddo
-  
+
 end program months
